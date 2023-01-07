a6b2ab5233649e6ffd17665798ab549926bb2e05
Prankul Verma <vprankul@gmail.com>
2023-01-07 14:55:43 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.getElementById("nav_btn").addEventListener("click", function () {
+  console.log("clicked");
+});
+
+// const nav = document.getElementB("nav_btn");
+
+// console.log(nav);
+
+// const toggle = document.getElementsByClassName("toggle")[0];
+
+// toggle.addEventListener("click", function () {
+//   document.getElementsByClassName("toggle")[0].style.backgroundColor = "black";
+// });
